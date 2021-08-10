a18a34b6dbe9d7dc8882d81e9ab863346dd38142
Salman-Khatib <81506640+Salman-Khatib@users.noreply.github.com>
2021-08-10 14:17:52 +0300

    Banner

--- WhereTo/client/src/App.js
+++ WhereTo/client/src/App.js
@@ -1,5 +1,7 @@
+import { Box } from '@material-ui/core';
 import './App.css';
 import Header from './components/Header';
+import Home from './components/home/Home';
 
 
 
@@ -8,6 +10,9 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Box style={{marginTop:64}} >
+        <Home />
+      </Box>
     </div>
   );
 }
