0a73803191e38728b5feab392998646814fc5010
Salman-Khatib <81506640+Salman-Khatib@users.noreply.github.com>
2021-08-10 13:26:53 +0300

    creating header component

---
+++ WhereTo/client/src/components/Header.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import {AppBar , makeStyles, Toolbar, Typography} from '@material-ui/core';
+
+
+const useStyles = makeStyles({});
+
+const Header = () => {
+    
+
+    
+    const classes = useStyles();
+    return (
+        <AppBar>
+            <Toolbar>
+                <Typography>HOME</Typography>
+                <Typography>ABOUT</Typography>
+                <Typography>CONTACT</Typography>
+                <Typography>LOGIN</Typography>
+            </Toolbar>
+        </AppBar>
+
+    )
+}
+
+export default Header;
