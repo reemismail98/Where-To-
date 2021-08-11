46d72f420299002e34fa73141af849a1f932c3df
Salman-Khatib <81506640+Salman-Khatib@users.noreply.github.com>
2021-08-11 09:36:27 +0300

    working on detailpage

--- WhereTo/client/src/App.js
+++ WhereTo/client/src/App.js
@@ -2,6 +2,7 @@ import { Box } from '@material-ui/core';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/home/Home';
+import DetailView from './components/post/DetailView';
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Box style={{marginTop:64}} >
         <Home />
+        <DetailView/>
       </Box>
       </>
   );
---
+++ WhereTo/client/src/components/post/DetailView.jsx
@@ -0,0 +1,14 @@
+
+
+
+import React from 'react'
+
+
+const DetailView = () => {
+    return(
+        <p>Hello</p>
+    )
+}
+
+
+export default DetailView;
